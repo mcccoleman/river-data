6f103154d60ca39385135b145230033ceed50f7a
Michael Coleman <colemanmc02@gmail.com>
2023-08-16 13:08:25 -0400

    return result as json

--- river_data.rb
+++ river_data.rb
@@ -1,10 +1,12 @@
 require_relative 'lib/river_data'
 require 'sinatra'
+require 'pry'
 
 get '/state/:code' do
   state_code = params[:code]
 
   resp = RiverData::State.find(state_code)
   filtered_data = Utils::GetTodaysValues.get(resp)
-  Utils::GroupByRiver.group(filtered_data)
+
+  Utils::GroupByRiver.group(filtered_data).to_json
 end
