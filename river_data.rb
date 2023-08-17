9d8c129936be0fd4dcb0a5a6259c6108663530a8
Michael Coleman <37200650+mcccoleman@users.noreply.github.com>
2023-08-17 07:56:02 -0400

    add site data (#14)

--- river_data.rb
+++ river_data.rb
@@ -1,7 +1,7 @@
 require_relative 'lib/river_data'
 require 'sinatra'
-require 'pry'
 require 'sinatra/cross_origin'
+require 'pry'
 
 configure do
   enable :cross_origin
@@ -11,11 +11,16 @@ options '/state/:code' do
   200
 end
 
-get '/state/:code' do
-  state_code = params[:code]
+get '/state/:stateCode' do
+  state_code = params[:stateCode]
 
   resp = RiverData::State.find(state_code)
   filtered_data = Utils::GetTodaysValues.get(resp)
 
   Utils::GroupByRiver.group(filtered_data).to_json
+end
+
+get '/site/:siteCode' do
+  site_code = params[:siteCode]
+  RiverData::Sites.find([site_code]).to_json
 end
