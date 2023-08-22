203f84c6d98b5797d245e1c671336089a0de329a
Michael Coleman <colemanmc02@gmail.com>
2023-08-21 20:42:08 -0400

    add status to response

--- river_data.rb
+++ river_data.rb
@@ -20,6 +20,10 @@ get '/state/:stateCode' do
   Utils::GroupByRiver.group(filtered_data).to_json
 end
 
+options '/site/:siteCode' do
+  200
+end
+
 get '/site/:siteCode' do
   site_code = params[:siteCode]
   RiverData::Sites.find([site_code]).to_json
